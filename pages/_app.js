7cc17d9b8453dd1897c1ce3ef07119fac4e0ea29
Yılmaz Çakmakçı <cakmakcy@gmail.com>
2020-07-02 21:14:16 +0300

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import Router from "next/router";
+import withGA from "next-ga"
 import 'normalize.css'
 import '../styles/app.scss'
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
     return (
         <Component {...pageProps} />
     )
 }
+
+export default withGA("UA-148352427-2", Router)(MyApp)
